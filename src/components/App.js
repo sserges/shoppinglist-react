df5d0f4dbd649bdd34cd13edba18e53c04b65849
sserges <agounkeserge@gmail.com>
2019-05-28 11:21:58 +0000

    App Initialization

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>List de courses</h3>
+        <Form />
+        <ItemList />
+      </div>
+    );
+  }
+}
+
+export default App;
