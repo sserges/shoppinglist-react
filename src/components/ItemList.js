57a6bc974bd35e24534a4506578f027462cc8337
sserges <agounkeserge@gmail.com>
2019-05-28 19:28:21 +0000

    display the contents of a table

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ItemList = () => {
-  return <div>ItemList component</div>;
+const ItemList = props => {
+  return (
+    <div>
+      {props.articles.map(article => (
+        <div key={article.id}>
+          {article.quantity} {article.name}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ItemList;
