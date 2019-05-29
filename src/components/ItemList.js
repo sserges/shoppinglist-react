d695d2f83788c4b101052ace1b42f77010792a43
sserges <agounkeserge@gmail.com>
2019-05-29 10:12:26 +0000

    Bootstrap added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,11 +3,14 @@ import React from "react";
 const ItemList = props => {
   return (
     <div>
-      {props.articles.map(article => (
-        <div key={article.id}>
-          {article.quantity} {article.name}
-        </div>
-      ))}
+      <h3>Votre liste de courses</h3>
+      <div>
+        {props.articles.map(article => (
+          <div key={article.id}>
+            {article.quantity} {article.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
