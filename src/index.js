f75c2f97e5c4a59c3b19ac13c69c50c6e5878274
sserges <agounkeserge@gmail.com>
2019-05-30 12:05:09 +0000

    creation of our first reducer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import { createStore, combineReducers } from "redux";
 
 import App from "./components/App";
 
-const store = createStore();
+const articlesReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ARTICLE":
+      console.log("ADD_ARTICLE");
+      console.log("action", action);
+      action.payload.id = Date.now();
+      const newState = [...state, action.payload];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    articles: articlesReducer
+  })
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
